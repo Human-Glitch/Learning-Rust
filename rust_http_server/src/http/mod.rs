c9143dc5b63106fbf39488c06c1ea1c6b6d1f380
Kody Buss <kody.buss@gmail.com>
2024-10-13 16:40:08 -0500

    stub out response struct

--- rust_http_server/src/http/mod.rs
+++ rust_http_server/src/http/mod.rs
@@ -1,8 +1,10 @@
 pub use method::Method;
 pub use request::Request;
+pub use response::Response;
 pub use request::ParseError;
 pub use query_string::{QueryString, Value};
 
 pub mod method;
 pub mod request;
 pub mod query_string;
+pub mod response;
