de449bb4a61740e31560c349fa4e0757d6aadfd5
Kody Buss <kody.buss@gmail.com>
2024-10-13 15:59:45 -0500

    Build a data structure for query string in a hashmap and abilities to write to it

--- rust_http_server/src/http/mod.rs
+++ rust_http_server/src/http/mod.rs
@@ -1,6 +1,8 @@
 pub use method::Method;
 pub use request::Request;
 pub use request::ParseError;
+pub use query_string::{QueryString, Value};
 
-mod method;
-mod request;
+pub mod method;
+pub mod request;
+pub mod query_string;
