a88b48c01b73f8dbd92629f41a1e9649e410a089
Kody Buss <Human-Glitch@users.noreply.github.com>
2024-12-11 22:58:01 -0600

    Delete new_variables directory

--- new_variables/target/debug/deps/new_variables-755cbdd1c3605399.d
+++
@@ -1,5 +0,0 @@
-c:\Repos\Rust\new_variables\target\debug\deps\new_variables-755cbdd1c3605399.rmeta: src\main.rs
-
-c:\Repos\Rust\new_variables\target\debug\deps\new_variables-755cbdd1c3605399.d: src\main.rs
-
-src\main.rs:
--- new_variables/target/debug/new_variables.d
+++
@@ -1,1 +0,0 @@
-C:\Repos\Rust\new_variables\target\debug\new_variables.exe: C:\Repos\Rust\new_variables\src\main.rs
