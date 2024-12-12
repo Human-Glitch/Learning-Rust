b828d463d0d23f6fd46a230922fbfacccf0a0f3e
Kody Buss <Human-Glitch@users.noreply.github.com>
2024-12-11 23:13:18 -0600

    Delete rust_demo directory

--- rust_demo/target/debug/deps/rust_demo-d5be2897e3ed0d1a.d
+++
@@ -1,5 +0,0 @@
-c:\Repos\Rust\rust_demo\target\debug\deps\rust_demo-d5be2897e3ed0d1a.rmeta: src\main.rs
-
-c:\Repos\Rust\rust_demo\target\debug\deps\rust_demo-d5be2897e3ed0d1a.d: src\main.rs
-
-src\main.rs:
--- rust_demo/target/debug/rust_demo.d
+++
@@ -1,1 +0,0 @@
-c:\Repos\Rust\rust_demo\target\debug\rust_demo.exe: c:\Repos\Rust\rust_demo\src\main.rs
